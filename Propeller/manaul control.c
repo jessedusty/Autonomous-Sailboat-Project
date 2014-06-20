ff08901ff1cf5ac6e7e2a09a6b1caab2d1098b44
Jesse Stevenson <jessedusty@gmail.com>
2014-06-20 13:41:44 -0400

    Added Compass Position Holding
    going to test this on the water soon

--- Propeller/manaul control.c
+++ Propeller/manaul control.c
@@ -2,6 +2,7 @@
 #include "fdserial.h"
 #include <string.h>
 #include "servo.h"
+#include "compass3d.h"
 
 fdserial *term;
 
@@ -10,6 +11,8 @@ fdserial *term;
 int rudderPosition, sailPosition;
 int lastRudderPosition, lastSailPosition;
 char new; 
+
+i2c *bus;
 
 int convertToInt(char * input) {
   return (int) strtol(input, (char **)NULL, 10);
@@ -42,6 +45,34 @@ void getSailStuff() {
   moveServos();
 }
 
+int compassEnabled;
+int x, y, z;
+float target;
+float heading; 
+
+void manageCompass() {
+  compassEnabled = 1;
+  compass_read(bus, &x, &y, &z);
+  float fy = (float) y;                     // Ints to floats
+  float fx = (float) x;
+  heading = atan2(fy, fx) * 180.0/PI; // Calculate heading with float
+  target = heading;
+  while (compassEnabled == 1) {
+    compass_read(bus, &x, &y, &z);
+    float fy = (float) y;                     // Ints to floats
+    float fx = (float) x;
+    heading = atan2(fy, fx) * 180.0/PI; // Calculate heading with floats
+    rudderPosition = 900 + (int)((heading - target) * 10);
+    if (rudderPosition < 100) rudderPosition = 100;
+    if (rudderPosition > 1250) rudderPosition = 1250;
+    moveServos();
+    pause(20);
+  }
+  cogstop(cogid());
+}
+
+
+
 void moveServos() {
   if (lastRudderPosition != rudderPosition) {
     servo_angle(2, rudderPosition);
@@ -57,6 +88,9 @@ int main()                                    // Main function
   servo_start(); 
   simpleterm_close();
 
+  bus = i2c_newbus(1, 0, 0);   
+  compass_init(bus);
+
   term = fdserial_open(9, 8, 0, 115200);
   new = 0; 
   while (1) {
@@ -65,8 +99,21 @@ int main()                                    // Main function
       case 'S':
         getSailStuff();         
         break;
+      case 'C':
+        if (compassEnabled == 0) {
+          dprint(term, "Maintianing Course\n");
+          cog_run(&manageCompass, 60);           
+        } else {
+          compassEnabled = 0;
+          dprint(term, "Regular Control\n");
+        }
+        break;
       default:
-        dprint(term, "Got something else \n");
+        if (compassEnabled == 1) {
+          dprint(term, "Current Rudder Postition: %d \n Target: %f \n Compass: %f\n", rudderPosition, target, heading);
+        } else {
+          dprint(term, "Got something else \n");
+        }
         break;
     }
   }
